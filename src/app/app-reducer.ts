3e0ad7f6355a5058af5c35dd67d0213881c711d1
Ilya Khrapko <iamdemos461@gmail.com>
2021-12-26 15:08:10 +0300

    add preloader, create app reducer, change task and todolist reducers

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,28 @@
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+const initialState = {
+    status: 'loading' as RequestStatusType
+}
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        default:
+            return state
+    }
+}
+
+//actions
+export const setAppStatusAC = (status: RequestStatusType) => ({
+    type: 'APP/SET-STATUS',
+    status
+} as const)
+
+
+//types
+export type setAppStatusActionType = ReturnType<typeof setAppStatusAC>
+type ActionsType = setAppStatusActionType
+
