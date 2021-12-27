bdd220c5cc7a4fdeb6af812e97c0fad29fd3c8ea
Ilya Khrapko <iamdemos461@gmail.com>
2021-12-27 20:41:51 +0300

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
